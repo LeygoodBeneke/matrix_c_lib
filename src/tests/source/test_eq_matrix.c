f6f1e03cab2ab41ab5342eb3620abbb729897664
Vasya <you@example.co>
2023-07-31 17:41:28 +0300

    Add equal matrix tests

--- src/tests/source/test_eq_matrix.c
+++ src/tests/source/test_eq_matrix.c
@@ -3,14 +3,43 @@
 START_TEST(eq_matrix_1) {
 	matrix_t matrix_1 = {}, matrix_2 = {};
 	int rows = 10, cols = 10;
-	int r_value = s21_create_matrix(rows, cols, &matrix_1) + s21_create_matrix(rows, cols, &matrix_1);
+	int r_value = s21_create_matrix(rows, cols, &matrix_1) + s21_create_matrix(rows, cols, &matrix_2);
 	FOR(rows, cols) {
 		matrix_1.matrix[i][j] = i + j;
 		matrix_2.matrix[i][j] = i + j;
 	}
-	r_value++;
-	//ck_assert_int_eq(r_value, 0);
-	//ck_assert_int_eq(s21_eq_matrix(&matrix_1, &matrix_2), 1);
+	ck_assert_int_eq(r_value, 0);
+	ck_assert_int_eq(s21_eq_matrix(&matrix_1, &matrix_2), 1);
+	s21_remove_matrix(&matrix_1);
+	s21_remove_matrix(&matrix_2);
+}
+END_TEST
+
+START_TEST(eq_matrix_2) {
+	matrix_t matrix_1 = {}, matrix_2 = {};
+	int rows = 10, cols = 10;
+	int r_value = s21_create_matrix(rows, cols, &matrix_1) + s21_create_matrix(rows, cols, &matrix_2);
+	FOR(rows, cols) {
+		matrix_1.matrix[i][j] = i + j;
+		matrix_2.matrix[i][j] = i * j;
+	}
+	ck_assert_int_eq(r_value, 0);
+	ck_assert_int_eq(s21_eq_matrix(&matrix_1, &matrix_2), 0);
+	s21_remove_matrix(&matrix_1);
+	s21_remove_matrix(&matrix_2);
+}
+END_TEST
+
+START_TEST(eq_matrix_3) {
+	matrix_t matrix_1 = {}, matrix_2 = {};
+	int rows = 10, cols = 10;
+	int r_value = s21_create_matrix(rows, cols, &matrix_1) + s21_create_matrix(rows, cols + 1, &matrix_2);
+	FOR(rows, cols) {
+		matrix_1.matrix[i][j] = i + j;
+		matrix_2.matrix[i][j] = i * j;
+	}
+	ck_assert_int_eq(r_value, 0);
+	ck_assert_int_eq(s21_eq_matrix(&matrix_1, &matrix_2), 0);
 	s21_remove_matrix(&matrix_1);
 	s21_remove_matrix(&matrix_2);
 }
@@ -21,6 +50,8 @@ Suite *test_eq_matrix() {
   TCase *tc = tcase_create("eq_matrix_tcase");
 
   tcase_add_test(tc, eq_matrix_1);
+  tcase_add_test(tc, eq_matrix_2);
+  tcase_add_test(tc, eq_matrix_3);
 
   suite_add_tcase(s, tc);
   return s;
